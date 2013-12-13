f802c1d086e30162494901d16923b96981cf3177
sdlong <sdlong.jeng@gmail.com>
2013-12-14 06:08:34 +0800

    initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+# Ruby on Rails Study Sample
+
+I'm sdlong, it's my Ruby on Rails Study demo
+
+Version : ch.4 : group have member funtion, user can join & quit group, post need join group first
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Groupme::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
+  resources :groups do 
+  	member do
+      post :join
+      post :quit
+    end
+    resources :posts
+  end
+
+  root :to => "groups#index"
+end
